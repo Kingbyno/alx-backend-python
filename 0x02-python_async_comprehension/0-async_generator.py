e61278b855db93627a550c9b21f3c6673d32869d
Kingbyno <kingbyno007@gmail.com>
2023-10-11 00:12:20 +0000

    Async Genrator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+import asyncio
+import random
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield ramdom.randint(0, 10)
+
+async def main():
+    async for number in async_generator():
+        print(number)
+if __name__ == "main":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
