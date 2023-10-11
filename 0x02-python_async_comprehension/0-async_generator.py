5048e806e1ab39e37f03661fd8317002d23f91b0
Kingbyno <kingbyno007@gmail.com>
2023-10-11 00:22:48 +0000

    Async Genrator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,15 +3,8 @@ import asyncio
 import random
 from typing import Generator
 
-async def async_generator():
+async def async_generator()Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield ramdom.randint(0, 10)
-
-async def main():
-    async for number in async_generator():
-        print(number)
-if __name__ == "main":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
 
